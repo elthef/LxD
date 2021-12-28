b09bfd9b098a377457a10efb2e37223d60f7e0cd
Edwin Foo <elthef@hotmail.com>
2021-12-28 09:26:05 +0800

    Updated file by THEF

---
+++ dfrobot_fit0458/dfrobot_fit0458.ino
@@ -0,0 +1,75 @@
+#include "pitches.h"
+
+const byte encoder0pinA = 2;//A pin -> the interrupt pin 0
+const byte encoder0pinB = 3;//B pin -> the digital pin 3
+byte encoder0PinALast;
+int duration;//the number of the pulses
+boolean Direction;//the rotation direction
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup()
+{
+  Serial.begin(115200);//Initialize the serial port
+  EncoderInit();//Initialize the module
+    // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(BDPIN_BUZZER, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(BDPIN_BUZZER);
+  }
+}
+
+void loop()
+{
+  Serial.print("Pulse:");
+  Serial.println(duration);
+  duration = 0;
+  delay(100);
+}
+
+void EncoderInit()
+{
+  Direction = true;//default -> Forward
+  pinMode(encoder0pinB,INPUT);
+  attachInterrupt(0, wheelSpeed, CHANGE);
+}
+
+void wheelSpeed()
+{
+  int Lstate = digitalRead(encoder0pinA);
+  if((encoder0PinALast == LOW) && Lstate==HIGH)
+  {
+    int val = digitalRead(encoder0pinB);
+    if(val == LOW && Direction)
+    {
+      Direction = false; //Reverse
+    }
+    else if(val == HIGH && !Direction)
+    {
+      Direction = true;  //Forward
+    }
+  }
+  encoder0PinALast = Lstate;
+
+  if(!Direction)  duration++;
+  else  duration--;
+}
